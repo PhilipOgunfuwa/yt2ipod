44965897a0019737dfcc1f174e76b5df1154da9f
Philip Ogunfuwa <philipaogunfuwa@gmail.com>
2026-08-23 09:44:55 -0400

    General reconstruction of how we add songs and deal with memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // #include "include/httplib.h"
 #include "itdb.h"
+#include "include/ipod_stuff.h"
 #include "include/yt2ipod.h"
 #include <iostream>
 
@@ -9,52 +10,40 @@
 
 int main(int argc, char** argv) {
 
-    if (argc != 2) {
-        std::cout << "usage: prog /ipod/mnt/point\n";
+    if (argc == 1 || argc > 3) {
+        std::cout << "usage: prog /ipod/mnt/point /path/to/song.mp3";
         return -1;
     }
 
 
     std::string strMountPoint { argv[1] };
-    GError *pError = nullptr;
-    Itdb_iTunesDB *pIpodMusicDB = itdb_parse(strMountPoint.c_str(), &pError);
+    std::string strPathToSong { argv[2] };
 
-    std::vector<Track> tracks { get_tracks(pIpodMusicDB) };
-    std::vector<Playlist> playlists { get_playlists(pIpodMusicDB) };
+    std::vector<std::unique_ptr<Track>> *tracks { nullptr };
+    std::vector<std::unique_ptr<Playlist>> *playlists { nullptr };
+    Itdb_iTunesDB *piTunesDB { nullptr };
 
-    for (const auto& playlist : playlists) {
-        std::cout << playlist.m_strName << '\n';
+    gboolean bSuccess { setup(strMountPoint, piTunesDB, playlists, tracks) };
+
+    if (bSuccess) {
+        while (true) {
+            GError *pError { nullptr };
+
+            Itdb_Track *pTrack { add_new_track(
+                piTunesDB,
+                strMountPoint,
+                *(playlists->front()),
+                *(tracks->front()),
+                strPathToSong, 
+                &pError
+            )};
+
+
+            break;
+        }
     }
 
-    for (const auto& track : tracks) {
-        std::cout << track.m_strTitle << '\n';
-        std::cout << "path: " << track.m_strIpodPath << '\n';
-        std::cout << "track len: " << track.m_dTrackLen_ms << '\n';
-    }
-
-    Track track {
-        "E85 [Official Visualizer].mp3",
-        "Don Toliver",
-        "Octane",
-        "Rap",
-        "",
-        0,
-        TRASH_TRACK_ID,
-        TRUE,
-    };
-
-    const gchar *filepath { "resources/Don Toliver - E85 [Official Visualizer].mp3" };
-
-    Itdb_Track *pTrack { add_new_track(pIpodMusicDB, strMountPoint, playlists[0], track, filepath, &pError) };
-
-    if (pTrack) {
-        std::cout << "added to ipod :)\n";
-        std::cout << pTrack->ipod_path << '\n';
-        std::cout << pTrack->id << '\n';
-        itdb_write(pIpodMusicDB, &pError);
-    }
-
-    itdb_free(pIpodMusicDB);
+    shutdown(piTunesDB);
 
     return 0;
 }
--- src/yt2ipod.cpp
+++ src/yt2ipod.cpp
@@ -1,227 +1,95 @@
-#include "itdb.h"
 #include "include/yt2ipod.h"
+#include "include/ipod_stuff.h"
+#include <memory>
+#include <iostream>
+#include <string>
+#include <cassert>
 
-/// @brief Initialize a track
-/// @param strTitle 
-/// @param strArtist 
-/// @param strAlbum 
-/// @param strGenre 
-/// @param strIpodPath 
-/// @param dID 
-Track::Track(const gchar *strTitle,
-             const gchar *strArtist,
-             const gchar *strAlbum,
-             const gchar *strGenre,
-             const gchar *strIpodPath,
-             gint32 dTrackLen_ms,
-             guint32 dID,
-             gboolean bTransferred)
-    : m_strTitle { "" }
-    , m_strArtist { "" }
-    , m_strAlbum { "" }
-    , m_strGenre { "" }
-    , m_strIpodPath { "" }
-    , m_dTrackLen_ms { dTrackLen_ms }
-    , m_dID { dID }
-    , m_bTransferred { bTransferred }
-{
-
-    // Convert C-style string to std::strings if non null
-
-    if (strTitle)
-        m_strTitle = strTitle;
-
-    if (strArtist)
-        m_strArtist = strArtist;
-
-    if (strAlbum)
-        m_strAlbum = strAlbum;
-
-    if (strGenre)
-        m_strGenre = strGenre;
-
-    if (strIpodPath)
-        m_strIpodPath = strIpodPath;
-}
-
-/// @brief Initialize a playlist
-/// @param TrackIDs 
-/// @param strName 
-/// @param bIsSmartPL 
-/// @param dID 
-Playlist::Playlist(std::vector<guint32>&& TrackIDs, 
-                   const gchar *strName, 
-                   bool bIsSmartPL, 
-                   guint64 dID) 
-    : m_TrackIDs { TrackIDs }
-    , m_strName { "" }
-    , m_bIsSmartPL { bIsSmartPL }
-    , m_dID { dID }
-{
-    // Turn C-style strings to std::strings if non null
-    if (strName)
-        m_strName = (strName);
-}
-
-/// @brief Get playlists from iTunesDB
+/// @brief Sets up iTunesDB from mount point, and puts playlists and tracks into passed in vecs
+/// @param strMountPoint 
 /// @param pDB 
-/// @return array of playlists
-std::vector<Playlist> get_playlists(Itdb_iTunesDB *pDB) {
-    std::vector<Playlist> playlists {};
+/// @param playlists 
+/// @param tracks 
+/// @return True if setup was successful, and false in any other case
+gboolean setup(
+    const std::string& strMountPoint, 
+    Itdb_iTunesDB *pDB, 
+    std::vector<std::unique_ptr<Playlist>> *playlists,
+    std::vector<std::unique_ptr<Track>> *tracks
+)
+{
+    std::cout << "Entering yt2ipod setup\n";
+    assert(pDB == nullptr && "Passed a non nullptr Itdb_iTunesDB");
+    assert(playlists->size() == 0 && "Passed playlists that is already populated");
+    assert(tracks->size() == 0 && "Passed tracks that is already populated");
 
-    if (!pDB)
-        return playlists;
-
-    GList *pCurrentNode { pDB->playlists };
-
-    // Populate playlists from iTunesDB
-    while (pCurrentNode) {
-        Itdb_Playlist *pCurPlaylist { static_cast<Itdb_Playlist *>(pCurrentNode->data) };
-
-        if (!pCurPlaylist)
-            break;
-
-        std::vector<guint32> trackIDs = get_track_ids(pCurPlaylist);
-
-        // Make playlist (that front end will use)
-        Playlist playlist {
-            std::move(trackIDs),
-            pCurPlaylist->name,
-            pCurPlaylist->is_spl,
-            pCurPlaylist->id,
-        };
-
-        playlists.push_back(std::move(playlist));
-        
-        pCurrentNode = pCurrentNode->next;
+    if (pDB || playlists->size() || tracks->size()) {
+        std::cout << "Exiting yt2ipod setup\n";
+        return FALSE;
     }
 
-    return playlists;
-}
+    GError *pError { nullptr };
+    std::cout << "Opening iTunesDB from iPod mount point @ " << strMountPoint << '\n';
+    pDB = itdb_parse(strMountPoint.c_str(), &pError);
 
-/// @brief Get tracks from iTunesDB
-/// @param pDB 
-/// @return tracks from iTunesDB
-std::vector<Track> get_tracks(Itdb_iTunesDB *pDB) {
-    std::vector<Track> tracks {};
-
-    if (pDB == nullptr)
-        return tracks;
-
-    GList *pCurrentNode { pDB->tracks };
-
-    // Populate tracks
-    while (pCurrentNode) {
-        Itdb_Track *pCurTrack { static_cast<Itdb_Track *>(pCurrentNode->data) };
-
-        if (!pCurTrack)
-            break;
-
-        // Make track (that the front end will use)
-        Track track {
-            pCurTrack->title,
-            pCurTrack->artist,
-            pCurTrack->album,
-            pCurTrack->genre,
-            pCurTrack->ipod_path,
-            pCurTrack->tracklen,
-            pCurTrack->id,
-            FALSE, // Track doesn't need added to iTunesDB
-        };
-
-        tracks.push_back(std::move(track));
-
-        pCurrentNode = pCurrentNode->next;
+    if (!pDB || pError) {
+        std::cout << "Failed to open iTunesDB\n";
+        std::cout << "error: " << pError->message << '\n'; // might want to switch to using fprintf(stderr, char*fmt, ...) here
+        std::cout << "Exiting yt2ipod setup\n";
+        return FALSE;
     }
 
-    return tracks;
+    std::cout << "Successfully opened iTunesDB\n";
+    std::cout << "Getting track(s) from iTunesDB\n";
+    tracks = get_tracks(pDB);
+    std::cout << "total of " << tracks->size() << " track(s)\n";
+
+    std::cout << "Getting playlist(s) from iTunesDB\n";
+    playlists = get_playlists(pDB);
+    std::cout << "total of " << playlists->size() << " playlist(s)\n";
+
+
+    /*
+    I imagine server stuff will be here aswell
+    */
+
+
+    std::cout << "Successfully setup yt2ipod";
+    return TRUE;
 }
 
-/// @brief Adds track to iTunesDB and uses path to a string
+
+/// @brief Frees anything from iTunesDB
 /// @param pDB 
-/// @param strMountPoint
-/// @param track 
-/// @param strPathToSong
-/// @param pError
-/// @return Itdb_Track if successfully added to itdb else NULL we also set pError
-Itdb_Track *add_new_track(
-    Itdb_iTunesDB *pDB,
-    const std::string& strMountpoint,
-    Playlist& targetPlaylist,
-    Track& newTrack,
-    const std::string& strSrcSongPath,
-    GError **pError
-) 
-{
-    Itdb_Track *pTrack { nullptr };
+void shutdown(Itdb_iTunesDB *pDB) {
 
-    if (!pDB || *pError)
-        return pTrack;
+    std::cout << "Entering yt2ipod shutdown\n";
+    assert(pDB && "Passed a nullptr for iTunesDB");
 
-    pTrack = itdb_track_new();
+    if (pDB) {
+        const std::string& strMountPoint { itdb_get_mountpoint(pDB) };
+        std::cout << "Closing iTunesDB from iPod mount point @ " << strMountPoint << '\n';
 
-    if (!pTrack)
-        return pTrack; // will be a nullptr
+        // do final writes to iTunesDB
+        std::cout << "Writing to iTunesDB...\n";
+        GError *pError { nullptr };
+        gboolean bSuccess { itdb_write(pDB, &pError) };
 
-    // Set up pTrack with data from newTrack
-    pTrack->title = g_strdup(newTrack.m_strTitle.c_str());
-    pTrack->artist = g_strdup(newTrack.m_strAlbum.c_str());
-    pTrack->album = g_strdup(newTrack.m_strAlbum.c_str());
-    pTrack->genre = g_strdup(newTrack.m_strGenre.c_str());
-
-
-    // Add track to iTunesDB, and then update track to be linked to song and copy to iPod
-    itdb_track_add(pDB, pTrack, END_OF_ITUNESDB);
-
-    // Add track+song file to 
-    {
-        gboolean bSuccess { itdb_cp_track_to_ipod(pTrack, strSrcSongPath.c_str(), pError) }; 
-
-        // Update length of track (in ms)
         if (bSuccess) {
-            drmp3 song;
-            drmp3_init_file(&song, strSrcSongPath.c_str(), nullptr);
-            drmp3_uint64 dSongFrameCount { drmp3_get_pcm_frame_count(&song) }; // frame
-            drmp3_uint32 dSongBitRate { song.sampleRate }; // in seconds
-
-            // Size of song is frame count / bit rate
-            pTrack->tracklen = static_cast<gint32>(
-                (dSongFrameCount / dSongBitRate) * 1000
-            );
-
-            drmp3_uninit(&song);
-        }
-    }
-
-    // Add track to target playlist
-    Itdb_Playlist *pTargetPlaylist { itdb_playlist_by_id(pDB, targetPlaylist.m_dID) };
-    itdb_playlist_add_track(pTargetPlaylist, pTrack, END_OF_PL);
-
-    return pTrack;
-}
-
-/// @brief Get ids of tracks in Itdb_Playlist
-/// @param pPlaylist 
-/// @return ids of tracks 
-std::vector<guint32> get_track_ids(Itdb_Playlist *pPlaylist) {
-    std::vector<guint32> trackIDs {};
-
-    if (pPlaylist == nullptr)
-        return trackIDs;
-
-    GList *pCurrentNode = pPlaylist->members;
-
-    // Populate buffer of track ids for each track in playlist
-    while (pCurrentNode) {
-        Itdb_Track *pCurrentTrack = static_cast<Itdb_Track *>(pCurrentNode->data);
-
-        // Playlist has track(s)
-        if (pCurrentTrack) {
-            trackIDs.push_back(pCurrentTrack->id);
+            std::cout << "Successfully wrote to iTunesDB\n";
         }
 
-        pCurrentNode = pCurrentNode->next;
-    }
+        else {
+            std::cout << "Failed to write to iTunesDB\n";
+            std::cout << "error: " << pError->message << '\n';
+            /*
+            We should probably add a way to ask if someone wants to write to iTunesDB again??
+            */
+           std::cout << "Changes made since last iTunesDB write won't be applied\n";
+        }
 
-    return trackIDs;
+        itdb_free(pDB);
+    }
+    
+    std::cout << "Exiting yt2ipod shutdown\n";
 }
